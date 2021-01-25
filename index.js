72e195a196f6bf01514b12cfda552a7308939a37
kimjw2003 <kimjw111003@gmail.com>
2021-01-25 23:13:54 +0900

    [Study] more function

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 function sayHello(name, age){
-    console.log('Hello', name, age);
+    console.log(`Hello ${name} you are ${age} years old`);
+
+    return `Hello ${name} you are ${age} years old`;
 }
 
-sayHello("lee", 20);
-//console.log(sayHello());
+const greet = sayHello("lee", 20);
+console.log(greet);
+
+
+
+
+const calculator = {
+    plus: function(a, b){
+        return a+b;
+    }
+}
+
+const plus = calculator.plus(5, 5);
+console.log(plus);
