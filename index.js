7ce82022845a630709d55207453c0b0a06be5611
kimjw2003 <kimjw111003@gmail.com>
2021-01-25 20:12:24 +0900

    [Study] function, arguments

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-
-const KimInfo = {
-    name : "Kim", 
-    age : 19,
-    gender :  true,
-    city : "Korea",
-    favNum : ["123",
-     "456"],
-    favPerson : [
-        {
-            name: "Lee",
-            age: "19"
-        },
-        {
-            name: "Son",
-            age: "19"
-        }
-    ]
+function sayHello(name, age){
+    console.log('Hello', name, age);
 }
 
-console.log(KimInfo.favPerson[1].name);
+sayHello("lee", 20);
+//console.log(sayHello());
