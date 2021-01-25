8e5bf466e67f949d9e82d3915d11dcb232650028
kimjw2003 <kimjw111003@gmail.com>
2021-01-25 23:22:02 +0900

    [Work] calculator

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
-function sayHello(name, age){
-    console.log(`Hello ${name} you are ${age} years old`);
+// function sayHello(name, age){
+//     console.log(`Hello ${name} you are ${age} years old`);
 
-    return `Hello ${name} you are ${age} years old`;
-}
+//     return `Hello ${name} you are ${age} years old`;
+// }
 
-const greet = sayHello("lee", 20);
-console.log(greet);
-
-
+// const greet = sayHello("lee", 20);
+// console.log(greet);
 
 
 const calculator = {
     plus: function(a, b){
         return a+b;
+    },
+    minus: function(a, b){
+        return a-b;
+    },
+    mul: function(a, b){
+        return a*b;
+    },
+    div: function(a, b){
+        return a/b;
+    },
+    remainder: function(a, b){
+        return a%b;
+    },
+    power: function(a, b){
+        return a**b;
     }
 }
 
 const plus = calculator.plus(5, 5);
+const minus = calculator.minus(5, 5);
+const mul = calculator.mul(5, 5);
+const div = calculator.div(5, 5);
+const remainder = calculator.remainder(5, 5);
+const power = calculator.power(5, 5);
+
+
 console.log(plus);
+console.log(minus);
+console.log(mul);
+console.log(div);
+console.log(remainder);
+console.log(power);
