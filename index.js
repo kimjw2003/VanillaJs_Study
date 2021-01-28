1bc64f6c8cce25ef6b281b53e4e6539b41f124ee
kimjw2003 <kimjw111003@gmail.com>
2021-01-28 22:39:59 +0900

    [Study] DOM Functions

--- index.js
+++ index.js
@@ -1,45 +1,5 @@
-// function sayHello(name, age){
-//     console.log(`Hello ${name} you are ${age} years old`);
+const title = document.getElementById("title");
+title.innerHTML = "Hi";
 
-//     return `Hello ${name} you are ${age} years old`;
-// }
-
-// const greet = sayHello("lee", 20);
-// console.log(greet);
-
-
-const calculator = {
-    plus: function(a, b){
-        return a+b;
-    },
-    minus: function(a, b){
-        return a-b;
-    },
-    mul: function(a, b){
-        return a*b;
-    },
-    div: function(a, b){
-        return a/b;
-    },
-    remainder: function(a, b){
-        return a%b;
-    },
-    power: function(a, b){
-        return a**b;
-    }
-}
-
-const plus = calculator.plus(5, 5);
-const minus = calculator.minus(5, 5);
-const mul = calculator.mul(5, 5);
-const div = calculator.div(5, 5);
-const remainder = calculator.remainder(5, 5);
-const power = calculator.power(5, 5);
-
-
-console.log(plus);
-console.log(minus);
-console.log(mul);
-console.log(div);
-console.log(remainder);
-console.log(power);
+console.log(title);
+console.error("fuck");
