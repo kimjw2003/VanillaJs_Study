8fb21281629acabd6dabfb8b82212142507551f8
kimjw2003 <kimjw111003@gmail.com>
2021-02-20 13:02:19 +0900

    [Study] if, else, and, or

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const title = document.querySelector("#title");
-
-function handleClick(){
-    title.style.color = "red";
-    
+/*
+if("10" === 10){
+    console.log("yes");
+} else if("10" === "11"){
+    console.log("yes2");
+} else{
+    console.log("no")
 }
 
-window.addEventListener("click", handleClick);
+if(20 > 5 && "kim" === "kim"){
+    console.log("yes");
+} else{
+    console.log("no");
+}
+*/
+
+const age = prompt("How old are you?");
+if(age > 18 && age < 21){
+    console.log('you can drink');
+} else{
+    console.log('you cant drink');
+}
